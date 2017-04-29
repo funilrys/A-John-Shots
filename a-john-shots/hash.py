22908ccfe28bb4239edf02f19bf029ddaa4ac676
funilrys <contact@funilrys.com>
2017-04-29 21:36:09 +0200

    add Hash() under hash.py

--- a-john-shots/hash.py
+++ a-john-shots/hash.py
@@ -1,1 +1,9 @@
 #!/bin/env python
+
+
+class Hash(object):
+    """Return the hash of given file path with a given algorithm"""
+
+    def __init__(self, path, algorithm):
+        self.path = path
+        self.algorithm = algorithm
