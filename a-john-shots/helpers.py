fb2618f2640b8e333f942e3973478e9a5fdec758
funilrys <contact@funilrys.com>
2017-04-29 22:17:57 +0200

    add unset_empty()

--- a-john-shots/helpers.py
+++ a-john-shots/helpers.py
@@ -1,1 +1,10 @@
 #!/bin/env python
+
+
+def unset_empty(list_to_format):
+    """Delete all empty element(s) from a given lis
+
+    :param list_to_format: A list, List to format
+    """
+
+    return ' '.join(list_to_format).split()
