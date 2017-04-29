aa1dd564599c13b944f6ee4005c055d46ecfd00b
funilrys <contact@funilrys.com>
2017-04-29 21:05:09 +0200

    add algorith to argument of Core() into core.py

--- a-john-shots/core.py
+++ a-john-shots/core.py
@@ -4,5 +4,6 @@
 class Core(object):
     """A simple script to get Security Hash Algorithms into JSON format"""
 
-    def __init__(self, path):
+    def __init__(self, path, algorithm='sha512'):
         self.path = path
+        self.algorithm = algorithm
