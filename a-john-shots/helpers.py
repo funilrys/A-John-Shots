95ebba409bed746f0b4309b5a4272b16a22f22c2
funilrys <contact@funilrys.com>
2017-04-29 22:32:16 +0200

    add combine_dicts()

--- a-john-shots/helpers.py
+++ a-john-shots/helpers.py
@@ -8,3 +8,23 @@ def unset_empty(list_to_format):
     """
 
     return ' '.join(list_to_format).split()
+
+
+def combine_dicts(dict1, dict2):
+    """Combine two dictionnaries into one
+
+    :param dict1: A dict, First dict
+    :param dict2: A dict, Second dict
+    """
+
+    result = {}
+
+    for k, v in dict1.items():
+        if k in dict2.keys():
+            if isinstance(dict2[k], dict):
+                result[k] = combine_dicts(v, dict2.pop(k))
+        else:
+            result[k] = v
+    for k, v in dict2.items():
+        result[k] = v
+    return result
