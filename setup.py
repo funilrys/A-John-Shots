ceee44182b24ecdc0563a9e9a6841993d1978d0c
funilrys <contact@funilrys.com>
2017-04-30 19:54:55 +0200

    Rename + add long_description + update keywords

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name='aJohnShots',
+    name='a_john_shots',
     version="1.0.0",
     description='Python module/library for saving Security Hash Algorithms into JSON format.',
+    long_description=open('README').read(),
     author='funilrys',
     author_email='contact@funilrys.com',
     license='GPL-3.0 https://opensource.org/licenses/GPL-3.0',
     url='https://github.com/funilrys/A-John-Shots',
     platforms=['any'],
     packages=['a_john_shots'],
-    keywords=['Python', 'JSON', 'SHA 1',
-              'SHA-512', 'SHA-224', 'SHA-384', 'SHA'],
+    keywords=['Python', 'JSON', 'SHA-1',
+              'SHA-512', 'SHA-224', 'SHA-384', 'SHA', 'MD5'],
     classifiers=[
         'Environment :: Console',
         'Topic :: Software Development',
