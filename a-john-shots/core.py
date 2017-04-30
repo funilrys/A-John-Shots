938795237b1996336697f0c1f05e08dee7b2c756
funilrys <contact@funilrys.com>
2017-04-30 15:32:19 +0200

    add Core().get_file_under_directory() + add directory walk + review/rewrite Core().hierarchy()

--- a-john-shots/core.py
+++ a-john-shots/core.py
@@ -1,8 +1,8 @@
 #!/bin/env python
 from hash import Hash
-from helpers import unset_empty
+from helpers import unset_empty, combine_dicts
 from json import dumps
-from os import path
+from os import path, walk
 
 
 class Core(object):
@@ -14,6 +14,7 @@ class Core(object):
 
     def __init__(self, path, algorithm='sha512'):
         self.path = path
+        self.ppath = [self.path]
         self.algorithm = algorithm
 
     def hierarchy(self, hash_of_file):
@@ -23,25 +24,50 @@ class Core(object):
         """
 
         result = {}
-        local_result = result
 
-        hierarchy = unset_empty(self.path.split('/'))
-        for funilrys in hierarchy:
-            if funilrys == hierarchy[-1]:
-                local_result = local_result.setdefault(funilrys, hash_of_file)
-            else:
-                local_result = local_result.setdefault(funilrys, {})
+        for item in self.ppath:
+            hierarchy = unset_empty(item.split('/'))
+            local_result = result
+
+            for funilrys in hierarchy:
+                if funilrys == hierarchy[-1]:
+                    local_result = local_result.setdefault(
+                        funilrys, hash_of_file)
+                else:
+                    local_result = local_result.setdefault(funilrys, {})
+
+        return result
+
+    def get_file_under_directory(self, root, file):
+        """Get the file(s) which is/are under the current directory
+
+        :param root: A string, the current root
+        :param file: A string, the current file
+        """
+
+        result = {}
+        path_to_file = path.join(root, file)
+        self.ppath = unset_empty(path_to_file.split(self.path))
+        file_hash = Hash(path_to_file, self.algorithm).get()
+        result = self.hierarchy(file_hash)
 
         return result
 
     def get(self):
-        """Brain of the program"""
+        """Brain of the program. In charge to print the results into JSON format"""
 
         result = {}
         file_hash = {}
 
         if path.isfile(self.path):
             file_hash = Hash(self.path, self.algorithm, False).get()
-            result = self.hierarchy(file_hash)
+            result = self.hierarchy(file_hash, True)
+        elif path.isdir(self.path):
+            for root, dirs, files in walk(self.path):
+                for file in files:
+                    sub = self.get_file_under_directory(root, file)
+                    result = combine_dicts(sub, result)
+        else:
+            return None
 
         return dumps(result, indent=4)
