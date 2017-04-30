c79daa8959e64b88cafe78d061635eb85b196913
funilrys <contact@funilrys.com>
2017-04-30 19:42:07 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    name='aJohnShots',
+    version="1.0.0",
+    description='Python module/library for saving Security Hash Algorithms into JSON format.',
+    author='funilrys',
+    author_email='contact@funilrys.com',
+    license='GPL-3.0 https://opensource.org/licenses/GPL-3.0',
+    url='https://github.com/funilrys/A-John-Shots',
+    platforms=['any'],
+    packages=['a_john_shots'],
+    keywords=['Python', 'JSON', 'SHA 1',
+              'SHA-512', 'SHA-224', 'SHA-384', 'SHA'],
+    classifiers=[
+        'Environment :: Console',
+        'Topic :: Software Development',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+    ],
+)
+
+'''
+test_suite='testsuite',
+entry_points="""
+[console_scripts]
+cmd = package:main
+""",
+'''
