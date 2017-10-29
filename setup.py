8b97fd962df19cf0cb9d0fa18c5375358b0589f1
funilrys <contact@funilrys.com>
2017-10-29 08:37:24 +0100

    Update license header + Update version

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 #    A-John-Shots - Python module/library for saving Security Hash Algorithms into JSON format.
-#    Copyright (C) 2017  Funilrys - Funilrys - Nissar Chababy <contact at funilrys dot com>
+#    Copyright (C) 2017  Funilrys - Nissar Chababy <contact at funilrys dot com>
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
 
 #    Original Version: https://github.com/funilrys/A-John-Shots
 
@@ -20,7 +15,7 @@ from distutils.core import setup
 
 setup(
     name='a_john_shots',
-    version="1.0.3",
+    version="1.0.4",
     description='Python module/library for saving Security Hash Algorithms into JSON format.',
     long_description=open('README').read(),
     author='funilrys',
@@ -29,8 +24,15 @@ setup(
     url='https://github.com/funilrys/A-John-Shots',
     platforms=['any'],
     packages=['a_john_shots'],
-    keywords=['Python', 'JSON', 'SHA-1',
-              'SHA-512', 'SHA-224', 'SHA-384', 'SHA', 'MD5'],
+    keywords=[
+        'Python',
+        'JSON',
+        'SHA-1',
+        'SHA-512',
+        'SHA-224',
+        'SHA-384',
+        'SHA',
+        'MD5'],
     classifiers=[
         'Environment :: Console',
         'Topic :: Software Development',
@@ -39,8 +41,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
-    ],
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'],
 )
 
 '''
